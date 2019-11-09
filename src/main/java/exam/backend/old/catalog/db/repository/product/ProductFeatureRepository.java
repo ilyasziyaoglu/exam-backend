6b580abf52a2bcffd07d13ea1a0090f2a0b92339
ILYAS ZIYAOGLU <ilyas.ziyaoglu@consultant.turkcell.com>
2019-11-09 15:59:27 +0300

    deleted old entities

--- src/main/java/exam/backend/old/catalog/db/repository/product/ProductFeatureRepository.java
+++
@@ -1,9 +0,0 @@
-package exam.backend.old.catalog.db.repository.product;
-
-import exam.backend.old.catalog.db.entity.product.ProductFeature;
-import exam.backend.common.db.repository.BaseRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductFeatureRepository extends BaseRepository<ProductFeature> {
-}
