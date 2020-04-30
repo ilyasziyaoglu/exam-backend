1c63be517d3d1953fb25aa53fcd8f1b7b02ce886
ILYAS ZIYAOGLU <ilyasziyaoglu@gmail.com>
2020-04-30 20:26:59 +0300

    ...

--- src/main/java/exam/backend/catalog/db/repository/product/ProductFeatureRepository.java
+++
@@ -1,9 +0,0 @@
-package exam.backend.catalog.db.repository.product;
-
-import exam.backend.catalog.db.entity.product.ProductFeature;
-import exam.backend.common.db.repository.BaseRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductFeatureRepository extends BaseRepository<ProductFeature> {
-}
